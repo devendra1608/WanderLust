ce16073b8693719b53c9ecc059eb15842feb0171
devendra1608 <151811312+devendra1608@users.noreply.github.com>
2025-12-22 10:45:27 +0530

    added show listing route

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ async function main() {
 }
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
+app.use(express.urlencoded({extended: true}));
 app.get("/",(req,res) => {
   res.send("Hi, i am root");
 });
@@ -29,7 +30,12 @@ app.get("/listings",async (req,res) => {
   res.render("listings/index.ejs",{allListings});
 });
 
-
+// Show Listing Route
+app.get("/listings/:id", async (req,res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/show.ejs", { listing });
+});
 // app.get("/testListing", async (req,res) => {
 //   let sampleLisiting = new Listing({
 //     title : "My New Villa",
